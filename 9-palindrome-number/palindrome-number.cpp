fc79ea242bbfdcf06d13701dbbe9d86cd3080b01
vikasadapa14 <vikasadapa71@gmail.com>
2026-08-18 23:32:30 +0530

    Time: 4 ms (30.59%) | Memory: 8.4 MB (92.52%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        // Negative numbers are never palindromes
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        long long reversed = 0;
+
+        while (x > 0) {
+
+            int digit = x % 10;
+
+            reversed = reversed * 10 + digit;
+
+            x = x / 10;
+        }
+
+        return original == reversed;
+    }
+};
