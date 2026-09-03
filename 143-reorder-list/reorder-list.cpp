a05f22113647bb98035601c11e605c2e665110f9
vikasadapa14 <vikasadapa71@gmail.com>
2026-09-03 12:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (26.23%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return;
+
+        // 1. Find the middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // 2. Reverse the second half
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+
+        ListNode* prev = nullptr;
+
+        while (second != nullptr) {
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
+        }
+
+        second = prev;
+
+        // 3. Merge the two halves
+        ListNode* first = head;
+
+        while (second != nullptr) {
+            ListNode* firstNext = first->next;
+            ListNode* secondNext = second->next;
+
+            first->next = second;
+            second->next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
+        }
+    }
+};
